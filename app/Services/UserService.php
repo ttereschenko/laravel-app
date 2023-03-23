19d263742456a96110636811a2b6698c186e3f3b
Tereschenko Tanya <trschnkt@gmail.com>
2023-03-23 13:43:05 +0300

    add login

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,11 +3,22 @@
 namespace App\Services;
 
 use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Support\Facades\Auth;
 
 class UserService
 {
     public function store(array $data): User
     {
         return User::create($data);
+    }
+
+    public function login(array $credentials): ?Authenticatable
+    {
+        if (Auth::attempt($credentials)) {
+            return auth()->user();
+        }
+
+        return null;
     }
 }
--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,9 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Support\Facades\Artisan;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
@@ -21,5 +21,44 @@ class AuthTest extends TestCase
         ];
 
         $this->postJson(route('register'), $data)->assertStatus(Response::HTTP_CREATED);
+    }
+
+    public function test_user_login(): void
+    {
+        $user = User::create([
+            'email' => $this->faker->email,
+            'password' => '12345678',
+        ]);
+
+        $this->postJson(route('login'), [
+            'email' => $user->email,
+            'password' => '12345678',
+        ])->assertCreated();
+    }
+
+    public function test_if_user_password_is_not_correct(): void
+    {
+        $user = User::create([
+            'email' => $this->faker->email,
+            'password' => '12345678',
+        ]);
+
+        $this->postJson(route('login'), [
+            'email' => $user->email,
+            'password' => 'qwerty',
+        ])->assertUnauthorized();
+    }
+
+    public function test_if_user_email_is_not_correct(): void
+    {
+        User::create([
+            'email' => $this->faker->email,
+            'password' => '12345678',
+        ]);
+
+        $this->postJson(route('login'), [
+            'email' => 'test@test',
+            'password' => '12345678',
+        ])->assertUnauthorized();
     }
 }
